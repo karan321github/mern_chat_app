4eb0b1855dfd23fa0dd0035f0ed6438420d697d2
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-04-26 16:18:41 +0530

    change endpoint

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mern-chat-app-98vj.onrender.com",
     // credentials: true,
   },
 });
